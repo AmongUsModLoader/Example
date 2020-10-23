5daf73c1d6d0505a05d44002f0241c4f6c5b2323
George Kazanjian <soggymustacheofficialmods@gmail.com>
2020-10-23 18:58:14 -0400

    Initia l

---
+++ AmongUsExampleMod/AmongUsExampleMod/ExampleMod.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using AmongUs.ModLoader;
+
+namespace AmongUsExampleMod {
+	
+	public class ExampleMod : Mod{
+		
+		public ExampleMod() : base("Example", "Example Mod", "0.0") { }
+		
+		
+	}
+}
