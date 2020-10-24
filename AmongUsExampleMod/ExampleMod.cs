a2025a2a53781fe3105b985f1dc3b88fbe96bda5
Ashley Wright <ahmedtrg981106@gmail.com>
2020-10-24 07:40:23 +0300

    Cleanup mod example

---
+++ AmongUsExampleMod/ExampleMod.cs
@@ -0,0 +1,15 @@
+﻿using AmongUs.Api;
+using AmongUs.ModLoader;
+
+namespace AmongUsExampleMod
+{
+	public class ExampleMod : Mod
+	{
+		public ExampleMod() : base("Example", "Example Mod", "0.0") {}
+
+		public override void Load()
+		{
+			Game.StartEvent += manager => Log.LogInfo("success");
+		}
+	}
+}
